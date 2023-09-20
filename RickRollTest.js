41703fa9eca72065c3260c3aedb2754747b5bddc
Kole Dallam <kolejasonD@gmail.com>
2023-09-20 11:36:45 -0600

    autoplay policy changes

--- RickRollTest.js
+++ RickRollTest.js
@@ -1,14 +1,17 @@
-const { Builder } = require('selenium-webdriver');
+const { Builder, By, Key } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 (async function playVideo() {
-    let driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options()).build();
+    // Set Chrome options to allow autoplay without user interaction considering most of the internet has a non autoplay policy i guess.
+    // had to manually turn off the autoplay policy that most people have enabled. below shown is the script turning it off when ran so it always plays. :D
+    const options = new chrome.Options();
+    options.addArguments("--autoplay-policy=no-user-gesture-required");
+
+    let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
 
     try {
         await driver.get('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
 
-        // Give some time for the video page to load and then click the video to play since wasn't playing in my last commit.
-        // changed click action to spacebar since often times users have "spacebar" as a pause play toggle.
         await driver.sleep(2000); 
         await driver.findElement(By.css('body')).sendKeys(Key.SPACE);
 
