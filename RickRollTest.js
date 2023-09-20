bfc7d8b9d0d3d2d657421bbddacaa292650b556c
Kole Dallam <kolejasonD@gmail.com>
2023-09-20 12:06:36 -0600

    tried swapping to iframes and theater mode no luck

--- RickRollTest.js
+++ RickRollTest.js
@@ -6,30 +6,24 @@ const chrome = require('selenium-webdriver/chrome');
     options.addArguments("--autoplay-policy=no-user-gesture-required");
     options.addArguments("--incognito");
     options.addArguments("--disable-background-networking");
+    options.addArguments("--start-maximized");
 
     let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
 
     try {
         await driver.get('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
 
-        const mainWindowHandle = await driver.getWindowHandle();
+        let iframeElement = await driver.wait(until.elementLocated(By.css('iframe')), 10000);
+        await driver.switchTo().frame(iframeElement);
 
-        await driver.sleep(2000); 
+        await driver.sleep(2000);
         await driver.findElement(By.css('body')).sendKeys(Key.SPACE);
 
         await driver.sleep(2000);
-        let videoElement = await driver.findElement(By.css('video'));
-        await videoElement.click();  // Set focus on the video element before double-clicking. If not executed; selenium will lose grip on the window and error out.
-        
-        let actions = driver.actions();
-        await actions.doubleClick(videoElement).perform();
-
-        // Switch back to the main window handle.
-        await driver.sleep(2000);
-        await driver.switchTo().window(mainWindowHandle);
+        let theaterModeButton = await driver.findElement(By.css('button[title="Theater mode"]'));
+        await theaterModeButton.click();
 
     } catch (error) {
         console.error('Error occurred:', error);
     }
 })();
-
