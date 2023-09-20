4b8c3c71c0827f82428a1fdb179e25327a2d44d2
Kole Dallam <kolejasonD@gmail.com>
2023-09-20 11:23:22 -0600

    simple test complete

--- RickRollTest.js
+++ RickRollTest.js
@@ -1,2 +1,15 @@
-const { Builder, By } = require('selenium-webdriver');
+const { Builder } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+
+(async function playVideo() {
+    let driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options()).build();
+
+    try {
+        await driver.get('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
+    } catch (error) {
+        console.error('Error occurred:', error);
+    } finally {
+        // terminates browser after test is ran.
+        // await driver.quit();
+    }
+})();
